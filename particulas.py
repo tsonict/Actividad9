6b728ad95d703a090d45ed30fa901f364bec3ed9
Pedro Flores <pedro.luquin7468@alumnos.udg.mx>
2020-10-08 19:52:51 -0500

    aÃdicion de funciones de prueba

--- particulas.py
+++ particulas.py
@@ -14,10 +14,14 @@ class Particulas:
         for particula in self.__list_particulas:
             print(particula)
 
+
+
 p01 = Particula(id=123, origen_x= 20, origen_y= 25, destino_x= 70, destino_y= 30, velocidad= 14123, red= 10, green= 255, blue= 255)
+p02 = Particula(id=222, origen_x= 34, origen_y= 21, destino_x= 100, destino_y= 7, velocidad= 23984, red= 60, green= 0, blue= 0)
 
 
 particulas = Particulas()
 
 particulas.agregar_final(p01)
+particulas.agregar_inicio(p02)
 particulas.mostrar()
